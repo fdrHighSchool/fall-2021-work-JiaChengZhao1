95f1290e67a0dadb2e66d6f4263ec25e44a97611
Jia Cheng Zhao <jiachengzhao@ac1mwk0f4jc9h.78b505.nycboe.org>
2021-09-27 08:18:09 -0400

    week 2 work

---
+++ 01-perfect-day/PerfectDay.java
@@ -0,0 +1,38 @@
+public class PerfectDay {
+  public static void main(String[] args) {
+    System.out.println("Here's my perfect day:");
+  }//end main method
+  public static void morning() {
+    System.out.print("In the morning");
+    System.out.println("I would wake up late and skip breakfast");
+    System.out.println("I will play games and watch videos for a hour");
+  }//end morning method
+
+  public static void afternoon() {
+    System.out.println("At 2:00 I would go hang out with friends");
+    System.out.print("We would go get food at a Korean BBQ place, ");
+    System.out.println("then we would go play 8 ball pool");
+  }//end afternoon method
+
+  public static void night() {
+    System.out.println("At 6:00 or 7:00, I would go home and shower");
+    System.out.print("Afterwards, I would try to do something productive, ");
+    System.out.print("such as drawing, exercising or studying");
+    System.out.println("Finally, I would play around for a bit more before I sleep");
+  }//end night method
+}//end class (first line)
+
+//in the morning I would wake up at 12:00
+//I would skip breakfast
+//I'll watch videos and play some games on my phone for about a hour
+/*
+Around 2:00 or so I would go out to hang out with friends.
+We would get food at a Korean BBQ place
+After we eat, we would go play 8 ball poo
+*/
+
+/*
+At around 6 or 7, I'd return home and shower before anything else.
+I would try to do something more productive than lazing around.
+I would draw, do some exercises or study for two hours.
+*/
