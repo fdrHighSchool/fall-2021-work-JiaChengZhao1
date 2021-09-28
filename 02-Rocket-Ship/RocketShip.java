c178df07a4d470fb15be147715e8b227be18bff0
JiaChengZhao1 <jiachengz3@nycstudents.net>
2021-09-28 08:47:00 -0400

    started on rocketship

---
+++ 02-Rocket-Ship/RocketShip.java
@@ -0,0 +1,22 @@
+public class RocketShip {
+  public static void main(String[] args) {
+    Triangle();
+    Square();
+    UnitedStates();
+    Square();
+    Triangle();
+  }//end main method
+
+
+  public static void Triangle(){
+    System.out.print("    /\\");
+    System.out.print("   /   \\");
+    System.out.print("  /      \\");
+  }//end triangle method
+
+  public static void Square(){
+    System.out.print("  +------+")
+    System.out.print("  |      |")
+    System.out.print("  |      |")
+  }
+}
