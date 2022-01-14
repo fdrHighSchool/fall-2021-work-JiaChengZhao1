8b2336d993b31bd79ac40ae1966669f167fa4c3d
JiaChengZhao1 <jiachengz3@nycstudents.net>
2022-01-14 08:47:28 -0500

    started

---
+++ 07-Averages ETC/triplet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class triplet {
+  public static void main(String[] args) {
+      int a = 100
+      if (a > 100 & a < 200; a++) {
+
+      }
+  }
+}
---
+++ ConnectFour.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class ConnectFour {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    String[][] board = new String[6][7];
+
+    //System.out.println(Arrays.deepToString(board));
+    fillBoard(board);
+    displayBoard(board);
+    //PlayerAction(board);
+  } // end main method
+  public static void PlayerAction(String[][] board){
+    System.out.println("Please pick a column")
+    String action = s.nextLine();
+    if(input.contains("1")){
+      //output x on the first column
+    }
+
+
+  }
+
+
+
+  public static void fillBoard(String[][] board) {
+    for(int row = 0; row < board.length; row++) {
+      for(int col = 0; col < board[row].length; col++) {
+        board[row][col] = "[ ]";
+      } // end inner loop
+    } // end outer loop
+  } // end fillBoard method
+
+  public static void displayBoard(String[][] board) {
+    for(int row = 0; row < board.length; row++) {
+      for(int col = 0; col < board[row].length; col++) {
+        System.out.print(board[row][col] + " ");
+      } // end inner for loop
+      System.out.println();
+    } // end outer for loop
+  } // end displayBoard method
+
+
+} // end class
